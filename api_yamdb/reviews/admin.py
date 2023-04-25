aae983cc5ea56d07d4e73c5ff13dffd52bab1114
Vitaliy <v-2841@yandex.ru>
2023-04-25 13:32:35 +0400

    added models and admins panel

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    User, Title, Category, Genre, GenreTitle,
+    Review, Comment
+)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+    )
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+    )
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'year',
+        'category',
+    )
+    search_fields = ('name',)
+    list_filter = (
+        'category',
+    )
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'username',
+        'email',
+    )
+    search_fields = ('text',)
+    list_filter = ('role',)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(GenreTitle)
+admin.site.register(Review)
+admin.site.register(Comment)
