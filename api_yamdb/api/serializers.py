72a31709f118147da433fbf4d65bbb97e81f7ce9
Aleksei Kashtanov <kashtanov-2001@list.ru>
2023-04-27 18:28:23 +0400

    serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,67 @@
 from rest_framework import serializers
-from reviews.models import User
+from reviews.models import User, Category, Genre, Title, Review, Comment
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True)
+    title = serializers.SlugRelatedField(
+        slug_field='name', queryset=Title.objects.all())
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True)
+    review = serializers.PrimaryKeyRelatedField(
+        queryset=Review.objects.all())
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class TokenSerializer(serializers.ModelSerializer):
@@ -9,10 +71,3 @@ class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'confirmation_code', )
-
-
-class SignUpSerializer(serializers.ModelSerializer):
-    """Сериализатор для получение кода подтверждения."""
-    class Meta:
-        model = User
-        fields = ('email', 'username')
