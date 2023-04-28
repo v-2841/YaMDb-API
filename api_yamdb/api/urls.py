60ea506e7437caf0239c039fb06ac3e66ad12b3c
Aleksei Kashtanov <kashtanov-2001@list.ru>
2023-04-28 16:16:29 +0400

    api/urls2

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,27 +1,28 @@
-from django.urls import include, path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
-from .views import (CommentList, ReviewList, TitleList,
-                    GenreList, CategoryList, SignUpView,
-                    GetTokenView)
-
+from .views import (UserList, UserDetail, CategoryList, CategoryDetail,
+                    GenreList, GenreDetail, TitleList, TitleDetail,
+                    ReviewList, ReviewDetail, CommentList, CommentDetail,
+                    SignUpView, GetTokenView)
 
 router_v1 = DefaultRouter()
-router_v1.register('title', TitleList)
-router_v1.register('genre', GenreList)
-router_v1.register('category', CategoryList)
-router_v1.register(
-    r'titles/(?P<title_id>[\d]+)/reviews',
-    ReviewList,
-    basename='reviews'
-)
-router_v1.register(
-    r'titles/(?P<title_id>[\d]+)/reviews/(?P<review_id>[\d]+)/comments',
-    CommentList,
-    basename='comments',
-)
+router_v1.register('users', UserList, basename='user')
+router_v1.register('categories', CategoryList, basename='category')
+router_v1.register('genres', GenreList, basename='genre')
+router_v1.register('titles', TitleList, basename='title')
+router_v1.register('reviews', ReviewList, basename='review')
+router_v1.register('comments', CommentList, basename='comment')
+
 urlpatterns = [
-    path('v1/auth/signup/', SignUpView.as_view(), name='sign_up'),
-    path('v1/auth/token/', GetTokenView.as_view(), name='get_token'),
-    path('v1/', include(router_v1.urls)),
+    path('', include(router_v1.urls)),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+    path('categories/<int:pk>/',
+         CategoryDetail.as_view(),
+         name='category-detail'),
+    path('genres/<int:pk>/', GenreDetail.as_view(), name='genre-detail'),
+    path('titles/<int:pk>/', TitleDetail.as_view(), name='title-detail'),
+    path('reviews/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
+    path('comments/<int:pk>/', CommentDetail.as_view(), name='comment-detail'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('get-token/', GetTokenView.as_view(), name='get-token'),
 ]
